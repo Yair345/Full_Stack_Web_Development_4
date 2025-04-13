4e35d1f0d73d233b24491adaeafdc15b14e13520
אוריאל מנגיסטו <umangist@g.jct.ac.il>
2025-04-13 21:44:45 +0300

    Add getShiftedKey function to handle key shift functionality in VirtualKeyboard

--- src/components/VirtualKeyboard/VirtualKeyboard.jsx
+++ src/components/VirtualKeyboard/VirtualKeyboard.jsx
@@ -41,6 +41,15 @@ function VirtualKeyboard({ onKeyPress, onDelete, onEnter, language, setLanguage 
     return language === "he" ? heLayout : enLayout;
   };
 
+  const getShiftedKey = (key) => {
+    const shiftedKeys = {
+      Q: "q", W: "w", E: "e", R: "r", T: "t", Y: "y", U: "u", I: "i", O: "o", P: "p",
+      A: "a", S: "s", D: "d", F: "f", G: "g", H: "h", J: "j", K: "k", L: "l",
+      Z: "z", X: "x", C: "c", V: "v", B: "b", N: "n", M: "m"
+    };
+    return shiftedKeys[key] || key;
+  }
+
   const handleKeyClick = (key) => {
     if (key === "Shift") {
       setShift((prev) => !prev);
